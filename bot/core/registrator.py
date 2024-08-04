221ffb235c6f877c0d10f562950b3e1ebfe5f7a4
maxxximgb <kruzakoalsl@gmail.com>
2024-08-04 22:13:22 +0300

    Update registrator.py

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,28 +1,33 @@
-from pyrogram import Client
+import asyncio
 
 from bot.config import settings
 from bot.utils import logger
+from bot.utils.default import DEFAULT_HEADERS, DEFAULT_FINGERPRINT
 from bot.utils.json_db import JsonDB
 from bot.utils.proxy import get_proxy_dict
-from bot.utils.default import DEFAULT_HEADERS, DEFAULT_FINGERPRINT
+from kivy.uix.label import Label
+from kivy.uix.progressbar import ProgressBar
+from pyrogram import Client
+from pyrogram.errors import SessionPasswordNeeded, PasswordHashInvalid, PhoneCodeInvalid
 
 
-async def register_sessions() -> None:
+async def register_sessions(app) -> int:
     API_ID = settings.API_ID
     API_HASH = settings.API_HASH
 
     if not API_ID or not API_HASH:
         raise ValueError("API_ID and API_HASH not found in the .env file.")
 
-    session_name = input('\nEnter the session name (Press Enter to exit): ')
+    session_name = await app.get_input('Ввод имени сессии', 'Назовите сессию', 'Название сессии')
 
     if not session_name:
-        return None
+        return 0
 
-    proxy = input('\nEnter proxy (Press Enter to continue): ')
+    proxy = ''
 
     proxy_dict = get_proxy_dict(proxy)
-
+    phone_number = await app.get_input('Ввод номера телефона', 'Номер телефона',
+                                       'Введите номер телефона на который зарегистрирован аккаунт телеграм')
     session = Client(
         name=session_name,
         api_id=API_ID,
@@ -30,11 +35,37 @@ async def register_sessions() -> None:
         workdir="sessions/",
         proxy=proxy_dict,
     )
+    l = Label(text='Подключение к серверам телеграм')
+    app.root.add_widget(l)
+    pb = ProgressBar(max=100)
+    app.root.add_widget(pb)
+    app.progress_bar.value = 50
+    await session.connect()
+    app.progress_bar.value = 100
+    await asyncio.sleep(1)
+    app.root.clear_widgets()
+    l = Label(text='Отправка кода')
+    app.root.add_widget(l)
+    pb = ProgressBar(max=100)
+    app.root.add_widget(pb)
+    app.progress_bar.value = 50
+    sent_code_info = await session.send_code(phone_number)
+    app.progress_bar.value = 100
 
-    async with session:
-        user_data = await session.get_me()
+    try:
+        phone_code = await app.get_input('Ввод кода', 'Код', 'Введите код полученный из телеграм')
+        await session.sign_in(phone_number, sent_code_info.phone_code_hash, phone_code)
+    except SessionPasswordNeeded:
+        password = await app.get_input('Ввод пароля', 'Пароль', 'Введите ваш пароль от телеграм аккаунта')
+        try:
+            await session.check_password(password)
+        except PasswordHashInvalid:
+            exit(1)
+    except PhoneCodeInvalid:
+        exit(1)
+    user_data = await session.get_me()
 
-    logger.success(f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}')
+    app.root.text = f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}'
 
     db = JsonDB("profiles")
 
@@ -47,3 +78,5 @@ async def register_sessions() -> None:
     }
 
     db.save_data(data)
+    await session.disconnect()
+    return 1
